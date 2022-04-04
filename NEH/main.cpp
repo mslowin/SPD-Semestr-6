e20fddd8352a149e6bfcc7386b432e32aa4c127a
Michał Słowiński <62020662+mslowin@users.noreply.github.com>
2022-04-04 09:49:08 +0200

    Dodano obsługę 120 zestawów danych

--- NEH/main.cpp
+++ NEH/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -7,31 +8,49 @@ int main()
 {
     string tmp; 
     ifstream data("neh.data.txt");
+    string s="data.",s1;
     int pom = 0;
-    int N, M;
-    int P[10000]; //zadania
-    int X[1000];  //kolejnosc
-    //--------------------------------------------------WCZYTYWANIE DANYCH
-    while(tmp!="data.000:") 
-        data>>tmp; 
-    data>>N>>M;
-    for (int j = 0; j < N * M; j++)
-        data >> P[j];
-    for (int j = 0; j < N; j++)
-        X[j] = j;
-    //--------------------------------------------------------------------
+    int N;          //ilosc zadan (np. w pierwszym zestawie wynosi 4)
+    int M;          //liczba maszyn (np. w pierwszym zestawie wynosi 3)
+    int P[10000];   //zadania
+    int X[1000];    //kolejnosc
 
+    for(int i = 0; i < 1; i++) {                  //petla for do iteracji po wszystkich 120 zbiorach danych
+    //TODO:    zamienic w petli for, żeby i < 121
 
-    //----------------------------------------------------WYPISANIE DANYCH
-    for(int i = 0; i < N * M; i++) {
-        cout << P[i] << " ";
-        pom++;
-        if(pom == 3) {
-            cout << endl; 
-            pom = 0;
+        //----------------------------------------------------------WCZYTYWANIE DANYCH
+        if(i < 10) {
+            s1 = "data.00" + to_string(i) + ":";
         }
-    }
-    //--------------------------------------------------------------------
+        else if(i < 100) {
+            s1 = "data.0" + to_string(i) + ":";
+        }
+		else
+		{
+			s1 = "data." + to_string(i) + ":";
+		}
+        while(tmp != s1) 
+            data >> tmp; 
+        data >> N >> M;
+        for (int j = 0; j < N * M; j++)
+            data >> P[j];
+        for (int j = 0; j < N; j++)
+            X[j] = j;
+        //----------------------------------------------------------------------------
 
+
+        //------------------------------------------------------------WYPISANIE DANYCH
+        for(int i = 0; i < N * M; i++) {
+            cout << P[i] << " ";
+            pom++;
+            if(pom == 3) {
+                cout << endl; 
+                pom = 0;
+            }
+        }
+        //----------------------------------------------------------------------------
+
+        //TODO: Algorytm szeregowania zadań neh
+    }
     return 0;
 }
