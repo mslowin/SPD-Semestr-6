9376729c5c6b4a5f689b0342a67818ad628c7fd0
mslowin <slowinski.michal@outlook.com>
2022-06-12 16:46:45 +0200

    Główna pętla schrage z podziałem od teraz działa w pętli while
    Działa dopóki funkcja czy_wszystkie_zadania_wykonane() zwróci true

--- Zajecia #5 Carlier/Carlier.cpp
+++ Zajecia #5 Carlier/Carlier.cpp
@@ -4,12 +4,13 @@
 
 using namespace std;
 
-int  max_time_of_jobs(int num_of_jobs, int *R, int *P, int *Q);
-int  min_num_from_an_array(int *array, int num_of_elements);
-int  max_num_from_an_array(int *array, int num_of_elements);
-int* sort_and_return_new_array(int *R, int num_of_elements);
-int  schrage(const int num_of_jobs, int *R, int *P, int *Q);
-int  schragePodzial(const int num_of_jobs, int *R, int *P, int *Q);
+int  max_time_of_jobs               (int num_of_jobs, int *R, int *P, int *Q);
+int  min_num_from_an_array          (int *array, int num_of_elements);
+int  max_num_from_an_array          (int *array, int num_of_elements);
+int* sort_and_return_new_array      (int *R, int num_of_elements);
+int  schrage                        (const int num_of_jobs, int *R, int *P, int *Q);
+int  schragePodzial                 (const int num_of_jobs, int *R, int *P, int *Q);
+bool czy_wszystkie_zadania_wykonane (const int num_of_jobs, int *avaliable_jobs);
 
 int main()
 {
@@ -17,13 +18,15 @@ int main()
         "data.005:","data.006:","data.007:","data.008:" };
     string tmp; 
     ifstream data("schr.data.txt");
+    // ifstream data("carl.data\\carl.data.txt");
+
     int N;
     int R[100];
     int P[100];
     int Q[100];
     int X[100];
 
-    for(int i = 0; i < 9; i++) //petla for sluzaca do przemieszczania sie pomiedzy ośmioma zbiorami danych
+    for(int i = 1; i < 2; i++) //petla for sluzaca do przemieszczania sie pomiedzy ośmioma zbiorami danych
     {
         while(tmp != s[i]) 
             data >> tmp; 
@@ -34,8 +37,8 @@ int main()
         const int num_of_jobs = sizeof(R)/sizeof(R[0]);   // dla przykładu podstawowego wynosi 7
 
         int Cmax;
-        Cmax = schrage(N, R, P, Q);
-        cout << Cmax << endl;
+        Cmax = schragePodzial(N, R, P, Q);
+        cout << endl << "Cmax = " << Cmax << endl << endl;
     }
 
     return 0;
@@ -65,50 +68,68 @@ int  schragePodzial(const int num_of_jobs, int *R, int *P, int *Q)
     int tmp_t = 0;
     int t = 0;
     int max_Q = 0;
+    int t_next;
     int max_Q_index = 0;
+    int krok = 0;
 
+    while(czy_wszystkie_zadania_wykonane(num_of_jobs, avaliable_jobs) == false)
+    // for(int krok = 0; krok < N; krok++)    // kolejnych chwil czasu jest 7 i po inich iterujemy
+    {
+        cout << endl;    
+        cout << "-----------------------------------" << krok << ":" << endl;
 
-        for(int krok = 0; krok < N; krok++)    // kolejnych chwil czasu jest 7 i po inich iterujemy
-        {
-            tmp_t = min_num_from_an_array(done_jobs_R, N);
+        tmp_t = min_num_from_an_array(done_jobs_R, N);
+        // cout << "t = " << t << endl;
+        // cout << "tmp_t = " << tmp_t << endl;
 
-            // jesli aktualna chwila jest mniejsza od poczatku jakiegokolwiek zadania
-            if(t < tmp_t)  // dla podstawowego przypadku wykonuje się tylko raz miedzy t = 6 i t = 10
-                t = tmp_t;
+        // jesli aktualna chwila jest mniejsza od poczatku jakiegokolwiek zadania
+        if(t < tmp_t)  // dla podstawowego przypadku wykonuje się tylko raz miedzy t = 6 i t = 10
+            t = tmp_t;
 
-            for(int i = 0; i < N; i++)    // stprawdzamy ile jest dostepnych zadan w danej chwili t
-                if(done_jobs_R[i] <= t)             // jezeli ktorekolwiek zadanie ma R mniejsze od t zapisujemy je
-                    avaliable_jobs[i] = 0;          // jezeli w danej chwili zadanie jest dostepne wstawiamy 0
-            
-            for(int i = 0; i < N; i++)    // w odpowiednie miejsca tabeli avaliable_jobs_Q wstawiamy czasy Q zadan
-                if(avaliable_jobs[i] == 0)          // dostepnych w danej chwili
-                    avaliable_jobs_Q[i] = Q[i];
+        for(int i = 0; i < N; i++)    // stprawdzamy ile jest dostepnych zadan w danej chwili t
+            if(done_jobs_R[i] <= t)             // jezeli ktorekolwiek zadanie ma R mniejsze od t zapisujemy je
+                avaliable_jobs[i] = 0;          // jezeli w danej chwili zadanie jest dostepne wstawiamy 0
+        
+        for(int i = 0; i < N; i++)    // w odpowiednie miejsca tabeli avaliable_jobs_Q wstawiamy czasy Q zadan
+            if(avaliable_jobs[i] == 0)          // dostepnych w danej chwili
+                avaliable_jobs_Q[i] = Q[i];
 
-            max_Q = max_num_from_an_array(avaliable_jobs_Q, N);
+        max_Q = max_num_from_an_array(avaliable_jobs_Q, N);
+        // cout << "max_Q = " << max_Q << endl;
+        t_next = min_num_from_an_array(done_jobs_R, N);
+        cout << "t_next = " << max_Q << endl;
 
-            for(int i = 0; i < N; i++)              // iterujac przez wszystkie zadania sprawdzamy ktore zadanie jest tym z
-                if(avaliable_jobs_Q[i] == max_Q)    // najwiekszym Q
-                {
-                    max_Q_index = i;                // max_Q_index to indeks zadania z najwiekszym Q, ktore dodajemy do zadan wykonanych
-                    break;
-                }
-            
+        for(int i = 0; i < N; i++)              // iterujac przez wszystkie dostępne zadania sprawdzamy ktore zadanie jest tym z
+            cout << done_jobs_R[i] << " " ;
 
-            t = t + P[max_Q_index];     // modyfikujemy czas
-            X[krok] = max_Q_index;      // wrzerzucamy zadanie w kolejnosc
+        for(int i = 0; i < N; i++)              // iterujac przez wszystkie dostępne zadania sprawdzamy ktore zadanie jest tym z
+            if(avaliable_jobs_Q[i] == max_Q)    // najwiekszym Q
+            {
+                max_Q_index = i;                // max_Q_index to indeks zadania z najwiekszym Q, ktore dodajemy do zadan wykonanych
+                break;
+            }
+        
+        // cout << "dostepne zadania zaznaczone sa zerem:  (-1 <- tych jeszcze nie ma, 99 <- zakonczone)" << endl;
+        // for(int i = 0; i < N; i++)
+        //         cout << avaliable_jobs[i] << " ";
+        
+        // t++;
+        t = t + P[max_Q_index];     // modyfikujemy czas
+        X[krok] = max_Q_index;      // wrzerzucamy zadanie w kolejnosc
 
-            avaliable_jobs[max_Q_index] = 99;       // wykonane zadanie przyjmuje wartosc rozna od 0 i -1
-            avaliable_jobs_Q[max_Q_index] = -1;     // czas Q wykonanego zadania zmieniamy tak, żeby nie przeszkadzał przy wyliczaniu maksymalnego Q w kolejnych krokach
-            done_jobs_R[max_Q_index] = 9999;        // czas R wykonanego zadania zmieniamy tak, żeby nie zmieścił się w warunku if(done_jobs_R[i] <= t) 
-            C[max_Q_index] = t + Q[max_Q_index];    // obliczenie C dla dodawanego zadania
-        }
+        avaliable_jobs[max_Q_index] = 99;       // wykonane zadanie przyjmuje wartosc rozna od 0 i -1
+        avaliable_jobs_Q[max_Q_index] = -1;     // czas Q wykonanego zadania zmieniamy tak, żeby nie przeszkadzał przy wyliczaniu maksymalnego Q w kolejnych krokach
+        done_jobs_R[max_Q_index] = 9999;        // czas R wykonanego zadania zmieniamy tak, żeby nie zmieścił się w warunku if(done_jobs_R[i] <= t) 
+        C[max_Q_index] = t + Q[max_Q_index];    // obliczenie C dla dodawanego zadania
+        krok++;
+    }
 
-        // cout << endl << "kolejnosc uszeregowania to: " << endl;
-        // for(int i = 0; i < N; i ++)
-        //     cout << X[i] + 1 << " ";
+    // cout << endl << "kolejnosc uszeregowania to: " << endl;
+    // for(int i = 0; i < N; i ++)
+    //     cout << X[i] + 1 << " ";
 
-        Cmax = max_num_from_an_array(C, N);
-        // cout << endl << "Cmax = " << Cmax << endl << endl;
+    Cmax = max_num_from_an_array(C, N);
+    // cout << endl << "Cmax = " << Cmax << endl << endl;
 
     delete[] X;
     delete[] tab_RQ;
@@ -118,6 +139,21 @@ int  schragePodzial(const int num_of_jobs, int *R, int *P, int *Q)
     delete[] C;
 
     return Cmax;
+}
+
+bool czy_wszystkie_zadania_wykonane (const int num_of_jobs, int *avaliable_jobs) 
+{
+    int licznik = 0;
+    for(int i = 0; i < num_of_jobs; i++)
+    {
+        if(avaliable_jobs[i] == 99)     // jesli kolejne zadania są wykonane to dodajemy licznik
+            licznik++;
+    }
+    if(licznik == num_of_jobs)          // jesli wszystkie zadania wykonane, zwracamy true
+        return true;
+    // cout << "licznik = " << licznik << endl;
+
+    return false;
 }
 
 int  schrage(const int num_of_jobs, int *R, int *P, int *Q)
